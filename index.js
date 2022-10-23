607c3ab36393542e6c62614e7c81ca2b879cbfa7
Ahmed Tijani Akinfalabi <akinfalabiahmed@gmail.com>
2022-10-23 23:47:08 +0200

    commit

---
+++ index.js
@@ -0,0 +1,12 @@
+let entries = []
+
+function submitHandler (event) {
+    event.preventDefault()
+    const entry = Number(document.querySelector("#milesToday").value);
+    if (false) return;
+    document.querySelector("form").reset();
+    entries.push(entry)
+}
+
+const form = document.querySelector("form")
+form.addEventListener("submit", submitHandler())
