00fd43d0c2fb06846fe8dde62984861c870152db
Ahmed Tijani Akinfalabi <akinfalabiahmed@gmail.com>
2022-10-24 23:55:56 +0200

    commit

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
 let entries = []
 
+const entriesWrapper = document.querySelector("entries")
+
 function newEntry(newEntry) {
+//     entriesWrapper.removeChild(entriesWrapper.firstElementChild)
     const listItems = document.createElement("li")
+    const listValue = document.createTextNode(newEntry)
+    listItems.appendChild(listValue)
+    
+    entriesWrapper.appendChild(listItems)
 }
 
 function submitHandler (event) {
-    event.preventDefault()
+    // To prevent the browser from reloading which is the default thing to 
+    // do after submitting a form, you call the preventDefault().
+    event.preventDefault() 
     const entry = Number(document.querySelector("#milesToday").value);
-    if (false) return;
+
+    if (!entry) return;
     document.querySelector("form").reset();
     entries.push(entry)
+    newEntry(entry)
 }
 
 const form = document.querySelector("form")
-form.addEventListener("submit", submitHandler())
+form.addEventListener("submit", submitHandler)
